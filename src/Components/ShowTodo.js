65ed76bdfde5cf3afed78ca4cf339c856a369631
sp5255 <pundir5255@gmail.com>
2021-08-01 21:12:11 +0530

    add icons

--- src/Components/ShowTodo.js
+++ src/Components/ShowTodo.js
@@ -54,12 +54,18 @@ class ShowTodo extends Component {
             {data.todo}
           </div>
           <div className="buttons">
+            <div>
             <input
               type="checkbox"
               checked={data.isCompleted}
               onChange={() => this.handleCheck(ind)}
-            />
-            <button onClick={() => this.DeleteTask(ind)}> delete </button>
+            />            
+
+            </div>
+            <div onClick={() => this.DeleteTask(ind)}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>
+            </div>
+            {/* <button > delete </button> */}
           </div>
         </div>
       );
